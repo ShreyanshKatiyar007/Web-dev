a6f9b2e50f79bbb95677ca981b761b6c56e9f644
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-02-16 23:58:16 +0530

    Create script.js

---
+++ JS/introduction/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("hello world");
+console.log("hello everyone");
+console.log("hello js");
+
+var a = prompt("Enter a number:");
+var isTrue = confirm("Are you sure to blast your device");
+
+if (isTrue) {
+	console.log("Device is blasting");
+} else {
+	console.log("Device is not blasting");
+}
+
+console.log("Your number is" + a);
+
+document.title = "I am good";
+
+//document.body.style.backgroundColor = "red";
