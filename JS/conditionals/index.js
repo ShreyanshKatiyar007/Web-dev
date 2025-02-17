f193d5523c63a3fc5bf0546af818164e95e771f8
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-02-17 23:30:07 +0530

    Create index.js

---
+++ JS/conditionals/index.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+let age = 45;
+
+if (age > 18) {
+	console.log("you can vote");
+} else {
+	console.log("you cannot vote");
+}
+
+let a = 10;
+let b = 7;
+
+let c = a < b ? a * b : a - b;
+
+console.log(c);
