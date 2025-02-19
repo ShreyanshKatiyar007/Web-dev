101b7af0ee0983a760198ffe8ce413be85a2e972
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-02-19 23:49:36 +0530

    Create index.js

---
+++ JS/string/index.js
@@ -0,0 +1,23 @@
+let a = "shreyansh"
+
+console.log(a)
+console.log(a[1])
+//console.log(a[10])
+
+console.log(a.length)     // property
+
+let real_name = "Alex"
+let friends = "john"
+
+// inserting variables in a string using backtick is called string interpolation
+console.log(`his name is ${real_name} and his friends name is ${friends}`)              // template literals  
+
+let b = "Javascipt"
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+console.log(b.replace("J", "H"))
+console.log(b.concat(a))
+console.log(a.concat(" katiyar ",b))
