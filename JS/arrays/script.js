2f7223ed8c78fec47487977677c47416a4320016
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-02-20 23:18:39 +0530

    Create script.js

---
+++ JS/arrays/script.js
@@ -0,0 +1,23 @@
+let arr = [2, 4, 6, 7, 8];
+
+console.log(arr);
+console.log(arr.length);
+
+arr[1] = 10;
+console.log(arr[1]);
+
+console.log(arr, typeof arr);
+
+console.log(arr.toString()); // converts array into string
+
+console.log(arr.join("and")); // comma's will be replaced by 'and'
+
+/* let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.splice(1, 2));
+console.log(numbers.splice(1, 3));
+console.log(numbers.splice(1, 3, 222, 333));  */
+
+let s = Array.from("shreyansh")
+console.log(s)
+
+
