0cf4fe2f1d6d3d3c0de677a1db9958df3056cec8
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-02-25 21:53:11 +0530

    Create script.js

---
+++ JS/DOM/nodes-parents-children/script.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+
+document.body.firstElementChild;
+document.body.firstElementChild.childNodes;
+document.body.firstElementChild.children;
+document.body.firstElementChild.parentElement;
+
+document.body.firstElementChild.children[3].nextElementSibling;
+document.body.firstElementChild.children[3].previousElementSibling;
