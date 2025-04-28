5acf355de50d7b8941de9430aea0334650aa9e0e
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2025-04-28 12:56:10 +0530

    Create script.js

---
+++ JS/ASSIGNMENT/Ques1/script.js
@@ -0,0 +1,11 @@
+/*
+1. Add 2 numbers together! In your script, type in console.log(23 + 97). Running this should log 120.
+2. Do the same thing but add 6 different numbers together.
+3. Now log the value of the following expression: (4 + 6 + 9) / 77. The console should log approximately 0.24675.
+*/
+
+console.log(23 + 97)
+
+console.log(20 + 43 + 7 + 30 + 18 + 12)
+
+console.log((4 + 6 + 9)/77)
